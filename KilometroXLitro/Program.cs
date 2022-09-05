acb04f4dc175cc2a51d56aa7657e8c30f3f9ff58
SoriaAlvaro <alvarosoria.universidad@gmail.com>
2022-09-04 23:46:50 -0300

    punto 3 terminado

---
+++ KilometroXLitro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ProblemaTres {
+    class Program {
+        static void Main(String[] args){
+            System.Console.WriteLine("Kilometros por litros\n");
+            try {
+                System.Console.WriteLine("Kilometros: ");
+                int k = Convert.ToInt32(Console.ReadLine());
+                System.Console.WriteLine("Litros: ");
+                int l = Convert.ToInt32(Console.ReadLine());
+                System.Console.WriteLine($"{k} Kilometros recorridos / {l} litros consumidos = {k / l} Kilometros por litro");
+            }catch(DivideByZeroException ex){
+                System.Console.WriteLine("Error Divsion por 0");
+            }catch(OverflowException ex){
+                System.Console.WriteLine("Error, numero demaciado grande");
+            }catch(FormatException ex){
+                System.Console.WriteLine("Error no ingresaste un numero");
+            }catch(Exception ex){
+                System.Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
