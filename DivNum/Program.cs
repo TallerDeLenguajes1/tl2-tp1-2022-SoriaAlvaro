7157a2eb5b758fa086f33c261f9a6a8844493d16
SoriaAlvaro <alvarosoria.universidad@gmail.com>
2022-09-04 23:24:31 -0300

    punto 2 terminado

---
+++ DivNum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ProblemaDos {
+    class Program {
+        static void Main(String[] args){
+            System.Console.WriteLine("Dividir 2 numeros\n");
+            try {
+                System.Console.WriteLine("Num uno: ");
+                int numUno = Convert.ToInt32(Console.ReadLine());
+                System.Console.WriteLine("Num dos: ");
+                int numDos = Convert.ToInt32(Console.ReadLine());
+                System.Console.WriteLine($"{numUno} / {numDos} = {numUno / numDos}");
+            }catch(DivideByZeroException ex){
+                System.Console.WriteLine("Error Divsion por 0");
+            }catch(OverflowException ex){
+                System.Console.WriteLine("Error, numero demaciado grande");
+            }catch(FormatException ex){
+                System.Console.WriteLine("Error no ingresaste un numero");
+            }catch(Exception ex){
+                System.Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
