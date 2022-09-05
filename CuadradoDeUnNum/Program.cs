bba2fe27c2893555debd5def91f527acddc73074
SoriaAlvaro <alvarosoria.universidad@gmail.com>
2022-09-04 22:43:47 -0300

    cuadrado de un num y primer commmit

---
+++ CuadradoDeUnNum/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TrabajoPracticoUno {
+    class Program {
+        static void Main(String[] args){
+            System.Console.WriteLine("Cuadrado de un numero\n");
+            try{
+                int num = Convert.ToInt32(Console.ReadLine());
+                System.Console.WriteLine($"{num} ^ 2 = {num*num}");
+            }catch(FormatException ex){
+                System.Console.WriteLine("No ingresaste un numero");
+            }catch(Exception ex){
+                System.Console.WriteLine(ex);
+            }
+        }
+    }
+}
